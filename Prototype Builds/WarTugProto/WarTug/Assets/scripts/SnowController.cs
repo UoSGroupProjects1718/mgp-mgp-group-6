85f96134bc571e59c7bc9e6004da2b45cc119d63
Tom <tom.gibbs@hotmail.co.uk>
2018-02-12 00:16:02 +0000

    Prototyped game concepts
    Added functional prototypes created for design concepts:
    1-Initial Tug of War (landscape)
    2-Initial Tug of War (portrait, mobile ar)
    3-Mid screen object, tug of war (portrait, mobile ar)
    4-Mid screen object, subtract from (portrait, mobile ar)
    5-Flow objects, absorb (portrait, mobile ar)

---
+++ Prototype Builds/WarTugProto/WarTug/Assets/scripts/SnowController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowController : MonoBehaviour
+{
+    public float cloudSpeed = 1.6f;
+
+    void Start()
+    { }
+
+    //Transform moves clouds to right.
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.left * cloudSpeed * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0))
+            Destroy(gameObject);
+
+    }
+
+    //When gameobject moves offscreen it is destroyed.
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
