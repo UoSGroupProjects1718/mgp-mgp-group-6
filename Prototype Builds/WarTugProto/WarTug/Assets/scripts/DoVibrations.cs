9b5e750b743fbd6e33e2c91ed16d5785fb5f1c8b
Tom <tom.gibbs@hotmail.co.uk>
2018-02-19 01:24:19 +0000

    Vibration/Sound implementation
    Uses native plugin with scripts.

---
+++ Prototype Builds/WarTugProto/WarTug/Assets/scripts/DoVibrations.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoVibrations : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Vibration.Vibrate(100);
+            System.Console.WriteLine("IT IS VIBRATING");
+        }
+	}
+}
