663f2083ffb7cd5c9f336c1e3865f658aac8053f
Tom <tom.gibbs@hotmail.co.uk>
2018-02-27 01:03:32 +0000

    Updated Prototype 26.02.18
    Prototype with recent design iterations.

---
+++ Prototype Builds/UpdatedProto [26.02.18]/Group6/Assets/Scripts/DoVibrations.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoVibrations : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    { }
+    
+    public void Vibrate10()
+    {
+        Vibration.Vibrate(10);
+    }
+
+    public void Vibrate100()
+    {
+        Vibration.Vibrate(100);
+    }
+
+    public void Vibrate1000()
+    {
+        Vibration.Vibrate(1000);
+    }
+}
