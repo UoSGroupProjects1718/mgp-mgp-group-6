1c80bfcc23d52a7889d11ea4f9cf4f2b2e4ababf
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2018-03-03 21:22:05 +0000

    Pushed updated Unity Build and Android apk
    Updated Unity Build clearly marked v2 and older build of same date moved to archive folder. Changes to build have been made to allow for building of apk.

---
+++ Prototype Builds/Archive/Unity Projects/UnityPrototype [03.03.18]/Group6 Main Build/Assets/Scipts/CameraShake.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public float duration = 1.0f;
+    public float lightPower = 0.5f;
+    public float mediumPower = 1.2f;
+    public float heavyPower = 3.0f;
+    public float slowDownAmount = 5.0f;
+
+    public bool shouldShake;
+
+    public bool lightShake;
+    public bool mediumShake;
+    public bool heavyShake;
+
+    private Transform camera;
+    private Vector3 startPosition;
+    private float initialDuration;
+
+	void Start ()
+    {
+        camera = this.transform;
+        startPosition = camera.localPosition;
+        initialDuration = duration;
+	}
+	
+	void Update ()
+    {
+		if (shouldShake)
+        {
+            if (lightShake && duration > 0)
+            {
+                mediumShake = false;
+                heavyShake = false;
+                camera.localPosition = startPosition + Random.insideUnitSphere * lightPower;
+                duration -= Time.deltaTime * slowDownAmount;
+            }
+            else if (mediumShake && duration > 0)
+            {
+                lightShake = false;
+                heavyShake = false;
+                camera.localPosition = startPosition + Random.insideUnitSphere * mediumPower;
+                duration -= Time.deltaTime * slowDownAmount;
+            }
+            else if (heavyShake && duration > 0)
+            {
+                lightShake = false;
+                mediumShake = false;
+                camera.localPosition = startPosition + Random.insideUnitSphere * heavyPower;
+                duration -= Time.deltaTime * slowDownAmount;
+            }
+            else
+            {
+                shouldShake = false;
+                duration = initialDuration;
+                camera.localPosition = startPosition;
+            }
+        }
+	}
+}
