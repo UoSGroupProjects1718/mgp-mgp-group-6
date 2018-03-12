a0545a229755a3f0466bc710f016c0aa66636d9b
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2018-03-12 16:19:48 +0000

    Uploaded New Unity Build and Android SDK (Ver 1.0.1)
    Unity Build Updated with Player Settings Changes needed for Android Build

---
+++ Builds/Unity Prototype [V1.0.1]/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour
+{
+    [Header("Game Manager")]
+    public GameObject Empty;
+    GameManager _gameManager;
+
+    [Header("Coutdown UI")]
+    private float tempTime = 0;
+    public int timeLeft = 10;
+    public int timeLeftReset;
+    public float countdownFillDiv1 = 10;
+    private float countdown = 1f;
+    [Header("Coutdown UI P1")]
+    public Text timerTextP1;
+    public Image countdownFillP1;
+    public Image countdownFillBackgroundP1;
+    [Header("Coutdown UI P2")]
+    public Text timerTextP2;
+    public Image countdownFillP2;
+    public Image countdownFillBackgroundP2;
+
+    void Start ()
+    {
+        _gameManager = Empty.GetComponent<GameManager>();
+        timerTextP1.enabled = false;
+        countdownFillP1.enabled = false;
+        countdownFillBackgroundP1.enabled = false;
+        timerTextP2.enabled = false;
+        countdownFillP2.enabled = false;
+        countdownFillBackgroundP2.enabled = false;
+    }
+	
+	void Update ()
+    {
+        if (_gameManager.theGameTurn == GameManager.GameTurn.Player1Turn)
+        {
+            tempTime += Time.deltaTime;
+            if (tempTime > 1.0f)
+            {
+                timeLeft--;
+                tempTime = 0;
+            }
+
+            timerTextP1.enabled = true;
+            countdownFillP1.enabled = true;
+            countdownFillBackgroundP1.enabled = true;
+            timerTextP2.enabled = false;
+            countdownFillP2.enabled = false;
+            countdownFillBackgroundP2.enabled = false;
+
+            countdown -= 1f / countdownFillDiv1 * Time.deltaTime;
+            countdownFillP1.fillAmount = countdown;
+
+            timerTextP1.text = ("" + timeLeft);
+
+            if (countdown <= 0)
+            {
+                timerTextP1.enabled = false;
+                countdownFillP1.enabled = false;
+                countdownFillBackgroundP1.enabled = false;
+                _gameManager.theGameTurn = GameManager.GameTurn.Player2Turn;
+                GameManager._boolPlayer1Turn = false;
+                timerTextP1.text = "0";
+                countdown = 1f;
+                timeLeft = timeLeftReset;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                timerTextP1.enabled = false;
+                countdownFillP1.enabled = false;
+                countdownFillBackgroundP1.enabled = false;
+                timerTextP1.text = "0";
+                countdown = 1f;
+                timeLeft = timeLeftReset;
+            }
+        }
+
+
+
+
+        if (_gameManager.theGameTurn == GameManager.GameTurn.Player2Turn)
+        {
+            tempTime += Time.deltaTime;
+            if (tempTime > 1.0f)
+            {
+                timeLeft--;
+                tempTime = 0;
+            }
+
+            timerTextP2.enabled = true;
+            countdownFillP2.enabled = true;
+            countdownFillBackgroundP2.enabled = true;
+            timerTextP1.enabled = false;
+            countdownFillP1.enabled = false;
+            countdownFillBackgroundP1.enabled = false;
+
+            countdown -= 1f / countdownFillDiv1 * Time.deltaTime;
+            countdownFillP2.fillAmount = countdown;
+
+            timerTextP2.text = ("" + timeLeft);
+
+            if (countdown <= 0)
+            {
+                timerTextP2.enabled = false;
+                countdownFillP2.enabled = false;
+                countdownFillBackgroundP2.enabled = false;
+                _gameManager.theGameTurn = GameManager.GameTurn.Player1Turn;
+                GameManager._boolPlayer1Turn = true;
+                timerTextP2.text = "0";
+                countdown = 1f;
+                timeLeft = timeLeftReset;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                timerTextP2.enabled = false;
+                countdownFillP2.enabled = false;
+                countdownFillBackgroundP2.enabled = false;
+                timerTextP2.text = "0";
+                countdown = 1f;
+                timeLeft = timeLeftReset;
+            }
+        }
+    }
+}
---
+++ Builds/Unity Prototype [V1.0.1]/Assets/Scripts/PushController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushController : MonoBehaviour
+{
+    private GameManager _gameManager;
+
+    [Header("Hierarchy Objects")]
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject punchPrefab;
+    public GameObject Empty;
+    private Rigidbody2D Player1rb;
+    private Rigidbody2D Player2rb;
+
+
+    void Start ()
+    {
+        _gameManager = Empty.GetComponent<GameManager>();
+        Player1rb = Player1.GetComponent<Rigidbody2D>();
+        Player2rb = Player2.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Debug.Log("punchControl: update");
+        if (GameManager._boolPlayer1Turn == true)
+        {
+            //Debug.Log("punchControl: transform punch down");
+            this.transform.Translate(-Vector3.up * 0.1f);
+        }
+        if (GameManager._boolPlayer1Turn == false)
+        {
+            //Debug.Log("punchControl: transform punch up");
+            this.transform.Translate(Vector3.up * 0.1f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(this.gameObject);
+    }
+}
