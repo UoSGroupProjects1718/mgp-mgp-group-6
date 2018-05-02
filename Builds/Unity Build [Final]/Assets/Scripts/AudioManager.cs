40b493ac0378b9cb4f06636ee37fd63b74214f69
Tom <tom.gibbs@hotmail.co.uk>
2018-05-02 11:29:17 +0100

    Folder names updated, final .apk uploaded.

---
+++ Builds/Unity Build [Final]/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] audioClip;
+
+    // have created assuming 8 sounds in array:
+
+    //index 0: bad tap
+    //index 1: good tap
+    //index 2: great tap
+    //index 3: perfect tap
+
+    //index 4: powerhit powerup activated
+    //index 5: extrachance powerup activated
+    //index 6: recover powerup activated
+
+    //index 7: timeout, change turn
+
+    //index 8: spit hit
+
+
+    void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+
+	void Update ()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    PlayAudio(1);
+        //}
+
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    PlayAudio(0);
+        //}
+	}
+
+    public void PlayAudio(int clip)
+    {
+        audioSource.PlayOneShot(audioClip[clip]);
+    }
+}
---
+++ Builds/Unity Build [Final]/Assets/Scripts/VibrationFunction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibrationFunction : MonoBehaviour
+{
+
+    void Start()
+    { }
+
+    void Update()
+    { }
+
+    //    public void Vibrate10()
+    //    {
+    //        Vibration.Vibrate(10);
+    //    }
+
+    //    public void Vibrate100()
+    //    {
+    //        Vibration.Vibrate(100);
+    //    }
+
+    //    public void Vibrate1000()
+    //    {
+    //        Vibration.Vibrate(1000);
+    //    }
+}
